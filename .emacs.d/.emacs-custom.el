f147dfaa1db788b7b27b464501657224dff42672
Sam Thomson <sammthomson@gmail.com>
2013-11-22 16:46:45 -0500

    add scala and javascript modes for emacs

--- .emacs.d/.emacs-custom.el
+++ .emacs.d/.emacs-custom.el
@@ -3,10 +3,19 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 215 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(global-show-newlines-mode t)
+ '(inhibit-startup-screen t)
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(text-mode-hook nil)
+ '(visual-line-mode nil t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ ;;'(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "apple" :family "Monaco")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "apple" :family "Monaco")))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,9 +4,26 @@
 
 ;; set emacs to interface with system clipboard
 (setq x-select-enable-clipboard t)
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+;;(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
 ;; show current column number
 (setq column-number-mode t)
 ;; keep backups out of the way
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
 (setq backup-by-copying t)
+
+;; JavaScript
+(add-to-list 'load-path "~/.emacs.d/js")
+(load "js3.elc")
+(setq auto-mode-alist (cons '("\\.json\\'" . js3-mode) auto-mode-alist))
+
+;; LKB mode
+(let ((root (or (getenv "DELPHINHOME")
+                  "/opt/delphin")))
+    (if (file-exists-p (format "%s/lkb/etc/dot.emacs" root))
+      (load (format "%s/lkb/etc/dot.emacs" root) nil t t)))
+
+;; scala mode
+(add-to-list 'load-path "~/.emacs.d/scala")
+(require 'scala-mode-auto)
+
+(load "/Users/sam/code/ducttape-0.3/tool-support/emacs/ducttape.el")
