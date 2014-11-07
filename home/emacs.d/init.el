dbd24728f4c8200cf71f22cb93135e5620ab6148
Sam Thomson <sammthomson@gmail.com>
2014-11-07 10:00:35 -0500

    add MELPA package management

--- home/emacs.d/init.el
+++ home/emacs.d/init.el
@@ -30,3 +30,10 @@
 (require 'scala-mode-auto)
 
 (load "~/code/ducttape/tool-support/emacs/ducttape.el")
+
+;; MELPA package management
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
