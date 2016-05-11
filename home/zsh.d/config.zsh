ae2d990737c7b09002cd6891e0bccb646583ceb8
Sam Thomson <sammthomson@gmail.com>
2016-05-11 15:22:33 -0700

    move *.sh scripts to *.zsh

--- home/zsh.d/config.zsh
+++ home/zsh.d/config.zsh
@@ -5,18 +5,13 @@ if [ -d ${EMACS_BACKUP_DIR} ]; then
         mkdir ${EMACS_BACKUP_DIR}
 fi
 
-# Ctr-arrow for moving by word in terminal
-# bind '"\e[5C": forward-word'
-# bind '"\e[5D": backward-word'
-# bind '"\e[1;5C": forward-word'
-# bind '"\e[1;5D": backward-word'
+bindkey '^[[1;5C' forward-word     # [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5D' backward-word    # [Ctrl-LeftArrow] - move backward one word
+
 
 export PATH="$PATH:$HOME/bin:$HOME/.cabal/bin"
 export PATH="/opt/scala/current/bin:$PATH"
 
-
-# show the current git branch
-#export PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
 
 ## from http://www.ukuug.org/events/linux2003/papers/bash_tips/
 ## shared history
