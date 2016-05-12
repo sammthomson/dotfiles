5af6d4502d4412edf65765632d4163750f1f141f
Sam Thomson <sammthomson@gmail.com>
2016-05-12 19:51:05 -0400

    module load git, tmux

--- home/zsh.d/cab.zsh
+++ home/zsh.d/cab.zsh
@@ -1,6 +1,7 @@
 module use /cab0/tools/modulefiles
+module load git-1.8.5.rc1
 module load python-2.7.3
-module load miscutil
+module load tmux-1.7
 module load htop-1.0.1
 module load scala-2.9.2
 module load R-3.1.0
