2aedd841498dbc01cebfba01f8448e616088b67b
Sam Thomson <sammthomson@gmail.com>
2016-05-13 11:53:21 -0400

    load emacs/24.5 module

--- home/zsh.d/cab.zsh
+++ home/zsh.d/cab.zsh
@@ -1,6 +1,7 @@
 module use /cab0/tools/modulefiles
+module load emacs/24.5
 module load git-1.8.5.rc1
-module load python-2.7.3
+module load anaconda2-4.0.0
 module load tmux-1.7
 module load htop-1.0.1
 module load scala-2.9.2
