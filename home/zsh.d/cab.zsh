d07abfc483f56ce6286937521f0a5bc48c292aa5
Sam Thomson <sammthomson@gmail.com>
2016-05-13 14:21:15 -0700

    closer to working on both cab and zin

--- home/zsh.d/cab.zsh
+++ home/zsh.d/cab.zsh
@@ -1,7 +1,7 @@
-module use /cab0/tools/modulefiles
+module use /opt/tools/modulefiles
 module load emacs/24.5
+module load python/anaconda3-4.0.0
 module load git-1.8.5.rc1
-module load anaconda2-4.0.0
 module load tmux-1.7
 module load htop-1.0.1
 module load scala-2.9.2
