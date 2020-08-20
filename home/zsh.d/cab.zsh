f7b4de48a51f234376ee2c8581ac90a88337718f
Sam Thomson <sammthomson@gmail.com>
2020-08-20 10:16:21 -0400

    rm cab.zsh

--- home/zsh.d/cab.zsh
+++
@@ -1,8 +0,0 @@
-module use /opt/tools/modulefiles
-module load emacs/24.5
-module load python/anaconda3-4.0.0
-module load git-1.8.5.rc1
-module load tmux-1.7
-module load htop-1.0.1
-module load scala-2.9.2
-module load R-3.1.0
